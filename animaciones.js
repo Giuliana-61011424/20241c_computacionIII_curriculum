bae8838debc6487be2cb7d26175f8b35f5040d53
Giuliana <belardo.giuliana@gmail.com>
2024-06-14 11:30:53 -0300

    pruebas

--- animaciones.js
+++
@@ -1,7 +0,0 @@
-function changeBg (){
-    var customheader = document.getElementById('custom-header');
-    var scrollValue =window.scrollY;
-    console.log(scroll.value);
-}
-
-window.addEventListener('scroll', changeBg);
